fc3521eae3b88eab9fb94fba4e27d3372f087b4d
Flop <uyweywgs@gmail.com>
2024-01-30 22:55:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# get-proxies
-Fast asynchronous SOCKS5 proxy fetcher and reliable double-checker. Designed for Proxychains. 
+# Get-Proxies
+
+<p align="center">
+  <img alt="Python3.10+ Only" src="https://img.shields.io/badge/Python_3.10%2B-Only-purple">
+  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/flop-code/get-proxies">
+  <img alt="GitHub License" src="https://img.shields.io/github/license/flop-code/get-proxies">
+  <img alt="Scrutinizer build" src="https://img.shields.io/scrutinizer/build/g/flop-code/get-proxies">
+  <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/flop-code/get-proxies">
+</p>
+
+
+**Get-Proxies** is an open-source, asynchronous, and flexible configuration tool to fetch SOCKS5 proxies and check them with multiple tests.
+
+The program **output format** is compatible with **Proxychains**, so you can quickly find working proxies and use them immediately.
+
+## Usage
+### Installing
+To start using **Get-Proxies**, you need to clone this repo with:
+```sh
+git clone https://github.com/flop-code/get-proxies.git
+```
+*[Optionally create a virtual environment]*
+
+And install requirements with:
+```sh
+pip install -r requirements.txt
+```
+
+### Running
+Run the program with:
+```sh
+python main.py
+```
+
+You can also use configuration files (in `.toml` format) to change proxy sources, delays, number of tests and more.
+
+By default, "`config.toml`" is used, but you can change it to your own, by running program with:
+```sh
+python main.py {path to your config file}
+```
+
+### Configuration
+To change the configuration you can either edit "`config.toml`" file or create your own (it should be in `.toml` format).
+
+**To get the description of each config parameter, look in [config.toml](config.toml).**
+
+Don't worry, if you skip one of the parameters or write it wrong, the program will warn you.
+
+## How does it work?
+The work at all stages is **asynchronous**, which makes the fetching and testing process really fast.
+
+The script fetches proxies from the sources _specified in the config file_, then runs a certain number of tests, with a certain delay between them to filter out non-working proxies.
+
+For example, you can use **2 tests with a delay of 3 minutes** between them, so the first test will weed out the initially non-working servers, and then identify more reliable and durable servers that are ready to work with them.
+
+By the same principle you can use **3 tests with a delay of 1.5 minutes** between them. Accordingly, there will be 3 stages of server screening, and unlike the previous example, there will be fewer servers at the last stage of testing (_if they will stop working during the interval between the 2nd and 3rd test_).
+The total test time will be no longer than the last example, but the network load will increase.
+
+**Each individual test works on the principle of sending a request to the HTTP server** (_also specified in the config file_) **through a certain proxy server**, and if the response code is the same as expected (_specified in the config file_), the test will be considered as passed. Otherwise, the proxy will be recognized as not working and will not participate in all subsequent tests.
+
+At the end of all tests, proxies recognized as working will be written to a separate file "`proxies.txt`" in a format ready to be copied into the Proxychains config file.
+
+### Screenshots
+
+
+<p align="center">
+  <img src="https://i.imgur.com/FO3KDGCl.jpg" alt="Normal screenshot" width="500"/>
+</p>
+
+
+### Educational purposes only
+
+*The author of the project is not responsible for the consequences of using this tool. Please use the tool and the result of its execution in accordance with the legislation of your country.*
---
+++ main.py
@@ -0,0 +1,183 @@
+from typing import Any
+from sys import argv
+from tomllib import load as toml_load, TOMLDecodeError
+import asyncio
+
+from colorama import Fore
+import aiohttp
+from aiohttp_socks import ProxyConnector
+
+
+class Log:
+    @staticmethod
+    def inf(msg: str) -> None:
+        print(Fore.LIGHTBLACK_EX + "INF\t" + Fore.RESET + msg)
+
+    @staticmethod
+    def ok(msg: str) -> None:
+        print(Fore.LIGHTGREEN_EX + "OK\t" + Fore.RESET + msg)
+
+    @staticmethod
+    def err(msg: str) -> None:
+        print(Fore.LIGHTRED_EX + "ERR\t" + Fore.RESET + msg)
+
+
+class GetProxies:
+    def __init__(self,
+                 sources: set[str],
+                 connection_timeout: int | float,
+                 number_of_tests: int,
+                 delay_between_tests: int | float,
+                 tests_url: str,
+                 expected_response_code: int,
+                 **kwargs):
+        self.sources = sources
+        self.connection_timeout = connection_timeout
+        self.number_of_tests = number_of_tests
+        self.delay_between_tests = delay_between_tests
+        self.tests_url = tests_url
+        self.expected_response_code = expected_response_code
+        self.__dict__.update(kwargs)
+
+    async def fetch_proxies(self) -> set[str]:
+        """Fetch proxies from provided sources.
+        Returns a set of strings (urls).
+        """
+        urls = set()
+
+        async with aiohttp.ClientSession() as session:
+            for url in self.sources:
+                try:
+                    async with session.get(url) as response:
+                        if response.status < 200 or response.status > 299:
+                            continue
+                        urls.update((await response.text()).split("\n"))
+                except (Exception,):
+                    Log.err(f"Error while fetching from \"{url}\".")
+
+        return urls
+
+    async def _test_url(self, url: str) -> tuple[str, bool]:
+        """Make requests to the test url.
+        Returns a tuple of string (proxy url) and boolean (`True` if proxy has passed all the tests, `False`, if not).
+        """
+        async with aiohttp.ClientSession(connector=ProxyConnector.from_url("socks5://" + url)) as session:
+            for test_number in range(self.number_of_tests):
+                try:
+                    async with session.get(
+                            self.tests_url,
+                            timeout=aiohttp.ClientTimeout(connect=self.connection_timeout)
+                    ) as response:
+                        if response.status == self.expected_response_code:
+                            if test_number != self.number_of_tests - 1:
+                                await asyncio.sleep(self.delay_between_tests)
+                        else:
+                            break
+                except (Exception,):
+                    break
+            else:
+                return url, True
+        return url, False
+
+    async def test_proxies(self, proxies: set[str]) -> set[str]:
+        """Test proxies.
+        Takes a set of strings (proxy urls).
+        Returns a set of strings (urls of proxies, that passed all the tests).
+        """
+        tasks = [self._test_url(url) for url in proxies]
+        results = await asyncio.gather(*tasks)
+        return {url for url, passed in results if passed}
+
+    @staticmethod
+    async def format_proxies(proxies: set[str]) -> str:
+        """Format proxies for Proxychains.
+        Takes a set of strings (proxy urls).
+        Returns a string of proxies in format for Proxychains.
+        """
+
+        proxy_list = []
+        for proxy in proxies:
+            proxy_list.append("socks5\t" + proxy.replace(":", "\t").expandtabs(17))
+
+        return "\n".join(proxy_list)
+
+
+class ConfigParser:
+    def __init__(self, config_filename: str):
+        self.filename = config_filename
+
+        with open(self.filename, "rb") as config_file:
+            try:
+                self._config = toml_load(config_file)
+            except TOMLDecodeError:
+                Log.err("Error while decoding config file. Exiting.")
+                quit(1)
+
+        self.CONFIG_SCHEMA = {
+            "io": {"sources": list, "output_filename": str},
+            "tests": {"number_of_tests": int, "tests_url": str, "expected_response_code": int,
+                      "connection_timeout": (int, float), "delay_between_tests": (int, float)}
+        }
+
+    def validate_config(self) -> str | None:
+        """Validate config.
+        Returns `None` if config is valid, otherwise return string (error message).
+        """
+        msg_prefix = "Config validation error:"
+
+        for section in self.CONFIG_SCHEMA:
+            # Check if section exists.
+            if section not in self._config:
+                return f"{msg_prefix} No \"[{section}]\" section."
+
+            for key in self.CONFIG_SCHEMA[section]:
+                # Check if item exists.
+                if key not in self._config[section]:
+                    return f"{msg_prefix} No \"{key}\" item in \"[{section}]\" section."
+
+                # Check if item has correct data type.
+                if not isinstance(self._config[section][key], self.CONFIG_SCHEMA[section][key]):
+                    return f"{msg_prefix} \"{key}\" item in \"[{section}]\" section has wrong data type."
+
+    @property
+    def config(self) -> dict[str, Any]:
+        return {**self._config["io"], **self._config["tests"]}
+
+
+async def main(args: list[str]) -> int:
+    Log.inf(Fore.LIGHTWHITE_EX + "Welcome to Get-Proxies!")
+
+    config_filename = args[0] if args else "config.toml"
+    Log.inf(f"Using config file \"{config_filename}\".\n")
+
+    config_parser = ConfigParser(config_filename)
+
+    validation_message = config_parser.validate_config()
+    if validation_message:
+        Log.err(validation_message)
+        return 1
+
+    get_proxies = GetProxies(**config_parser.config)
+
+    Log.inf("Fetching proxies...")
+    proxy_list = await get_proxies.fetch_proxies()
+    Log.ok(f"Fetched {len(proxy_list)} proxies.")
+
+    Log.inf("Testing proxies... (This may take some time)")
+    proxies = await get_proxies.test_proxies(proxy_list)
+    Log.ok(f"Tests completed, {len(proxies)} proxies passed.\n")
+
+    Log.inf("Writing to output file...\n")
+    with open(config_parser.config["output_filename"], "w") as output_file:
+        output_file.write(await GetProxies.format_proxies(proxies))
+
+    Log.ok("Done.")
+
+    return 0
+
+
+if __name__ == "__main__":
+    try:
+        exit(asyncio.run(main(argv[1:])))
+    except KeyboardInterrupt:
+        Log.inf("Ctrl+C detected. Bye.")
